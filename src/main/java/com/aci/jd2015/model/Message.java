58961ab5f19a676b0983100a15975ac8d0a84bcc
test <test@mail.ru>
2016-10-10 20:13:08 +0300

    This is my first commit

--- src/main/java/com/aci/jd2015/model/Message.java
+++ src/main/java/com/aci/jd2015/model/Message.java
@@ -6,6 +6,8 @@ public class Message implements Comparable<Message>{
 
 	private final Date datetime;
 	private final String message;
+	
+	// www
 
 	public Message(Date datetime, String message) {
 		this.datetime = datetime;
---
+++ src/main/java/com/aci/jd2015/model/NewClass.java
@@ -0,0 +1,10 @@
+package com.aci.jd2015.model;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
